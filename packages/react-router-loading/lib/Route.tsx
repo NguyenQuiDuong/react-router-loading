e3c07451c253a4dcd644fbdf6244898983004965
Nguyễn Quí Dương <duongnqse02934@gmail.com>
2022-12-02 16:40:13 +0700

    Update Route.tsx

--- packages/react-router-loading/lib/Route.tsx
+++ packages/react-router-loading/lib/Route.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import { Route as OriginalRoute, RouteProps as OriginalRouteProps } from 'react-router';
 
-interface RouteProps extends OriginalRouteProps {
+
+interface RouteProps  {
   loading?: boolean;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Route: FC<RouteProps> = ({ loading, ...props }) => <OriginalRoute {...props} />;
+const Route: FC<RouteProps & OriginalRouteProps> = ({ loading, ...props }) => <OriginalRoute {...props} />;
 
 export default Route;
